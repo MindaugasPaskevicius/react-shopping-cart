189e1ff2972553bfb9102a19b4a6210499f564b3
MindaugasPaskevicius <106962850+MindaugasPaskevicius@users.noreply.github.com>
2022-08-18 15:32:32 +0300

    Create Products folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import data from "./Components/Back/Data/Data";
+import Header from './Components/Front/Header/Header';
+import Routes from "./Components/Front/Routes/Routes";
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const App = () => {
+  const {productItems} = data;
+
   return (
     <div>
-    
+      <Router>
+        <Header />
+        <Routes />
+      </Router>
     </div>
   );
 };
